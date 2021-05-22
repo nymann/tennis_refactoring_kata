9cf236ead8fa0d1d4b3d446b4c5fd6614793e8f3
Kristian Nymann Jakobsen <kristian@nymann.dev>
2021-05-22 19:53:02 +0200

    :recycle: Extract score code into smaller procedural functions
    This is the first step.

--- src/tennis_scoreboard/tennis.py
+++ src/tennis_scoreboard/tennis.py
@@ -18,32 +18,42 @@ class TennisGame1:
         result = ""
         tempScore=0
         if (self.p1points==self.p2points):
-            result = {
-                0 : "Love-All",
-                1 : "Fifteen-All",
-                2 : "Thirty-All",
-            }.get(self.p1points, "Deuce")
+            result = self._equal()
         elif (self.p1points>=4 or self.p2points>=4):
-            minusResult = self.p1points-self.p2points
-            if (minusResult==1):
-                result ="Advantage " + self.player1Name
-            elif (minusResult ==-1):
-                result ="Advantage " + self.player2Name
-            elif (minusResult>=2):
-                result = "Win for " + self.player1Name
-            else:
-                result ="Win for " + self.player2Name
+            result = self._get_endgame_result()
         else:
-            for i in range(1,3):
-                if (i==1):
-                    tempScore = self.p1points
-                else:
-                    result+="-"
-                    tempScore = self.p2points
-                result += {
-                    0 : "Love",
-                    1 : "Fifteen",
-                    2 : "Thirty",
-                    3 : "Forty",
-                }[tempScore]
+            result = self._get_score()
+        return result
+
+    def _equal(self):
+        return {
+            0 : "Love-All",
+            1 : "Fifteen-All",
+            2 : "Thirty-All",
+        }.get(self.p1points, "Deuce")
+
+    def _get_endgame_result(self):
+        minusResult = self.p1points-self.p2points
+        if (minusResult==1):
+            return "Advantage " + self.player1Name
+        elif (minusResult ==-1):
+            return "Advantage " + self.player2Name
+        elif (minusResult>=2):
+            return  "Win for " + self.player1Name
+        return "Win for " + self.player2Name
+
+    def _get_score(self):
+        result = ""
+        for i in range(1,3):
+            if (i==1):
+                tempScore = self.p1points
+            else:
+                result+="-"
+                tempScore = self.p2points
+            result += {
+                0 : "Love",
+                1 : "Fifteen",
+                2 : "Thirty",
+                3 : "Forty",
+            }[tempScore]
         return result
