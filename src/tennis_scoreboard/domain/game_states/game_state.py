30c95ed3c5553721871df045b1951f8e3cf19f70
Kristian Nymann Jakobsen <kristian@nymann.dev>
2021-05-22 23:47:28 +0200

    :recycle: Mark GameState as abstract class that implements IGameState

--- src/tennis_scoreboard/domain/game_states/game_state.py
+++ src/tennis_scoreboard/domain/game_states/game_state.py
@@ -1,7 +1,8 @@
+from abc import ABC, abstractmethod
 from tennis_scoreboard.domain.game_states.i_game_state import IGameState
 from tennis_scoreboard.domain.players.i_player import IPlayer
 
-class GameState(IGameState):
+class GameState(IGameState, ABC):
     def __init__(self, p1: IPlayer, p2: IPlayer):
         self.p1 = p1
         self.p2 = p2
@@ -15,3 +16,6 @@ class GameState(IGameState):
     def _point_text(self, point: int):
         return ["Love", "Fifteen", "Thirty", "Forty"][point]
 
+    @abstractmethod
+    def scoreboard_format(self) -> str:
+        raise NotImplementedError()
